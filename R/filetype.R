5be9e19c8dceab03519ce5b5ad8426a23dd10417
Ben Raymond <ben@theraymonds.org>
2023-07-29 09:33:33 +1000

    add dv_file_data_type function v0.1.5

--- R/filetype.R
+++ R/filetype.R
@@ -1,8 +1,9 @@
 #' Detect scout file type
 #'
-#' Returns the general file type (format), but does not differentiate e.g. indoor from beach. Type is based on the file extension and a minimal check of the contents, but without fully parsing the file.
+#' Returns the general file type (format), but does not differentiate e.g. indoor from beach. Type is based on the file extension and a minimal check of the contents, but without fully parsing the file. For `dv_file_data_type` the type is based only on a minimal check of the contents (provided in `x`)
 #'
 #' @param filename character: paths to files. Files that do not exist will be classified as "unknown" type
+#' @param x character: a character vector with the contents of a scout file
 #' @param error_on_unknown logical: if `TRUE` an error will be thrown if an unknown file type is encountered
 #'
 #' @return Character vector containing:
@@ -16,30 +17,40 @@
 #' \dontrun{
 #'   library(ovdata)
 #'   dv_file_type(ovdata_example("190301_kats_beds")) ## dvw
+#'   x <- readLines(ovdata_example("190301_kats_beds"), warn = FALSE)
+#'   dv_file_data_type(x)
+#'
 #'   dv_file_type(ovdata_example("clickscout")) ## dvw
+#'   dv_file_data_type(readLines(ovdata_example("clickscout"), warn = FALSE))
+#'
 #'   dv_file_type(ovdata_example("2017_AVL_mens_HEAT_vs_UTSSU")) ## psvb
+#'   dv_file_data_type(readLines(ovdata_example("2017_AVL_mens_HEAT_vs_UTSSU"), warn = FALSE))
+#'
 #'   dv_file_type(tempfile(fileext = ".dvw")) ## unknown (contents not as expected for dvw)
+#'   dv_file_data_type(readLines(tempfile(fileext = ".dvw"))) ## "unknown", with warning
+#'
 #' }
 #' @export
 dv_file_type <- function(filename, error_on_unknown = FALSE) {
     assert_that(is.character(filename))
+    if_is <- function(x, ifis) if (identical(x, ifis)) x else "unknown"
     if (length(filename) == 1) {
         ext <- if (!file.exists(filename)) "" else tolower(tools::file_ext(filename))
         out <- if (!ext %in% c("dvw", "vsm", "xml", "psvb")) {
                    "unknown"
                } else if (ext == "dvw") {
                    chk <- tryCatch(readLines(filename, warn = FALSE, n = 1L), error = function(e) "")
-                   if (grepl("[3DATAVOLLEYSCOUT]", chk[1], fixed = TRUE)) "dvw" else "unknown"
+                   if_is(dv_file_data_type(chk), "dvw")
                } else if (ext == "vsm") {
                    ## minimal check that this is a json file, without actually parsing it
-                   chk <- suppressWarnings(tryCatch(readChar(filename, 2), error = function(e) ""))
-                   if (identical(chk, "{\"")) "vsm" else "unknown"
+                   chk <- suppressWarnings(tryCatch(readChar(filename, 500L), error = function(e) ""))
+                   if_is(dv_file_data_type(chk), "vsm")
                } else if (ext == "xml") {
                    chk <- readLines(filename, n = 200L, warn = FALSE)
-                   if (any(grepl("<ALL_INSTANCES>", chk, fixed = TRUE))) "hxml" else "unknown"
+                   if_is(dv_file_data_type(chk), "hxml")
                } else if (ext == "psvb") {
                    chk <- suppressWarnings(tryCatch(readChar(filename, 20), error = function(e) NULL))
-                   if (identical(base64enc::base64decode(chk)[5:6], as.raw(c(31, 8*16+11)))) "psvb" else "unknown"
+                   if_is(dv_file_data_type(chk), "psvb")
                } else {
                    "unknown"
                }
@@ -48,4 +59,23 @@ dv_file_type <- function(filename, error_on_unknown = FALSE) {
     } else {
         vapply(filename, dv_file_type, FUN.VALUE = "", USE.NAMES = FALSE)
     }
+}
+
+#' @rdname dv_file_type
+#' @export
+dv_file_data_type <- function(x, error_on_unknown = FALSE) {
+    tryCatch({
+        if (grepl("[3DATAVOLLEYSCOUT]", x[1], fixed = TRUE)) {
+            "dvw"
+        } else if (identical(substr(x[1], 1, 2), "{\"") && any(grepl("gameType", x), na.rm = TRUE)) {
+            ## minimal check that this is a json file, without actually parsing it
+            "vsm"
+        } else if (any(grepl("<ALL_INSTANCES>", x, fixed = TRUE), na.rm = TRUE)) {
+            "hxml"
+        } else if (identical(base64enc::base64decode(substr(x[1], 1, 10))[5:6], as.raw(c(31, 8*16+11)))) {
+            "psvb"
+        } else {
+            "unknown"
+        }
+    }, error = function(e) "unknown")
 }
--- tests/testthat/test-filetype.R
+++ tests/testthat/test-filetype.R
@@ -1,17 +1,32 @@
 test_that("file type detection works", {
     expect_identical(dv_file_type(ovdata::ovdata_example("190301_kats_beds")), "dvw")
+    expect_identical(dv_file_data_type(readLines(ovdata::ovdata_example("190301_kats_beds"))), "dvw")
+
     expect_identical(dv_file_type(ovdata::ovdata_example("clickscout")), "dvw")
+    expect_identical(dv_file_data_type(readLines(ovdata::ovdata_example("clickscout"))), "dvw")
+
     expect_identical(dv_file_type(ovdata::ovdata_example("2017_AVL_mens_HEAT_vs_UTSSU")), "psvb")
+    expect_identical(dv_file_data_type(readLines(ovdata::ovdata_example("2017_AVL_mens_HEAT_vs_UTSSU"))), "psvb")
+
     expect_identical(dv_file_type(tempfile(fileext = ".dvw")), "unknown")
     expect_identical(dv_file_type(tempfile(fileext = ".txt")), "unknown")
+    expect_identical(expect_warning(dv_file_data_type(readLines(tempfile(fileext = ".txt")))), "unknown")
+    expect_identical(dv_file_data_type(""), "unknown")
+    expect_identical(dv_file_data_type(NULL), "unknown")
+
     tf <- tempfile(fileext = ".vsm")
-    cat('{"gameType":"indoor","version":1,"startDate":"2022-11-04T04:56:28.681Z","scout":{"sets":[{"_id":"63649c8c62b3d751","startingLineup":{"home":{"positions"', file = tf)
+    temp <-  '{"gameType":"indoor","version":1,"startDate":"2022-11-04T04:56:28.681Z","scout":{"sets":[{"_id":"63649c8c62b3d751","startingLineup":{"home":{"positions"'
+    cat(temp, file = tf)
     expect_identical(dv_file_type(tf), "vsm")
     unlink(tf)
+    expect_identical(dv_file_data_type(temp), "vsm")
+
     tf <- tempfile(fileext = ".xml")
-    cat(c("blah", "blah", "<ALL_INSTANCES>"), file = tf, sep = "\n")
+    temp <- c("blah", "blah", "<ALL_INSTANCES>")
+    cat(temp, file = tf, sep = "\n")
     expect_identical(dv_file_type(tf), "hxml")
     unlink(tf)
+    expect_identical(dv_file_data_type(temp), "hxml")
 
     ## multiple files
     expect_identical(dv_file_type(c(ovdata::ovdata_example("190301_kats_beds"), tempfile(fileext = ".dvw"))), c("dvw", "unknown"))
